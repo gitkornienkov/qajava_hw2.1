ffd3cc0d87f8a83d44009e3e4651c5441cdb1928
pavelkornienkov <pikornienkov@gmail.com>
2023-02-12 17:41:37 +0300

    Заменил на камелкейс

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         double coast = 999.99; //Цена билета
-        int miles_per_coast = 20;
-        int miles = (int)coast / miles_per_coast;
+        int milesPerCoast = 20;
+        int miles = (int)coast / milesPerCoast;
         System.out.println("Цена билета: " + coast);
         System.out.println("Накоплено миль: " + miles);
     }
